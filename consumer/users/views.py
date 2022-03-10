baea30738452afb9fa2b524aa2afa4086d94788c
tenaciouscri <cridelve@gmail.com>
2022-03-10 07:29:53 +0100

    Executing sync code inside of async views

--- consumer/users/views.py
+++ consumer/users/views.py
@@ -1,16 +1,28 @@
 import httpx
+from asgiref.sync import sync_to_async  # [6]
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+
+def get_local_users():  # [7]
+    return list(User.objects.all())
 
 
 async def index(request):  # [1]
     async with httpx.AsyncClient() as client:  # [2]
         response = await client.get(
-            "http://localhost:8000/users/users/", auth=("admin", "password")
+            "http://localhost:8000/users/users/", auth=("admin", "weboverlord")
         )  # [3]
-    users = response.json()  # [4]
-    return render(request, "users/index.html", {"users": users})  # [5]
+    json = response.json()  # [4]
+    remote_users = json["results"]
+    local_users = await sync_to_async(get_local_users, thread_sensitive=True)()  # [8]
+    return render(
+        request,
+        "users/index.html",
+        {"remote_users": remote_users, "local_users": local_users},
+    )  # [5]
 
 
 # [1] This app does have views, but they're normal, asynchronous views.
@@ -22,3 +34,12 @@ async def index(request):  # [1]
 # will read that as JSON.
 # [5] Finally, it'll make users part of the context and render it with a template
 # (see users/index.html).
+
+# [6] A converter that makes having the synchronous inside the asynchronous possible.
+
+# [7] A function that does all the things that have to be done synchronously. In this
+# case, it'll return a list of all users.
+
+# [8] This is a synchronous call.
+# The "extra" set of parenthesis at the end of this line are there to call the
+# asynchronous function, which doesn't take any arguments.
